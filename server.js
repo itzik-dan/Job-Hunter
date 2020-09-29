1ca75586689b60afdc30d84dbcfb64d66d0362f2
tsahi  dan <tsahidan@tsahis-MacBook-Air.local>
2020-09-29 12:49:22 +0300

    third commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.json({ msg: "Welcome to the linkedin clone API" });
+	res.json({ message: "Welcome to Itzik linkedin clone API" });
 });
 
 const PORT = process.env.PORT || 5000;
